b321aa4ef3246add1c4b0c4c9c830c7015e3df7e
Karolina Jedrajczyk <kalka@MacBook-Pro-Karolina.local>
2021-01-18 09:53:27 +0100

    message

--- 04/app.js
+++ 04/app.js
@@ -13,45 +13,54 @@ console.log(randomArr(1,20));
 
 
 // kolejna funkcja ma przymować przez argument tablicę i sortować ją od wartości największej do najmniejszej. 
+
+function sortedArr(arr) {
+    const sorted = arr.sort(function(a,b) {
+        return b - a;
+    });
+    return sorted;
+}
 let arr2 = randomArr(1,100);
 
-arr2.sort(function(a,b) {
-    return b - a;
-});
+console.log(sortedArr(arr2));
 
-console.log(arr2);
+
+
 
 // Następnie ma zwrócić tablicę zawierającą tylko 10 największych liczb. W ciele funkcji możesz użyć metod `.sort()` oraz `.slice()`
-let biggest = arr2.slice(0,10);
-console.log(biggest);
+
+function biggestTen(arr) {
+    const biggest10 = arr.slice(0,10);
+    return biggest10;
+}
+
+console.log(biggestTen(sortedArr(arr2)));
+
 
 
 // Ostatnia funkcja ma obliczać [średnią arytmetyczną]
-let sum = biggest.reduce(function(a,b) {
-    return (a+b);
-});
-let average = sum /  biggest.length;
-console.log(average);
 
 
+function getAvg(arr) {
+    let sum = arr.reduce(function(a,b) {
+        return (a+b);
+    });
+    let average = sum /  arr.length;
+    return average;
+    }
+    
+
+
+   
+console.log(getAvg(biggestTen(sortedArr(arr2))));
 
 
 // Wykorzystaj napisane funkcje w taki sposób, aby utworzyć tablicę składającą się z 20 losowych liczb z przedziału od 10 do 200. Z utworzonej tablicy wybiesz 10 największych i obliczych ich średnią arytmetyczną.
 
 
-let arr3 = randomArr(10,200);
+let oneTwoHundred = randomArr(1,200);
 
-arr3.sort(function(a,b) {
-    return b - a;
-});
-console.log(arr3);
-
-
-let biggest3 = arr3.slice(0,10);
-console.log(biggest3);
-
-let sum3 = biggest3.reduce(function(a,b) {
-    return (a+b);
-});
-let average3 = sum3 /  biggest3.length;
-console.log(average3);
+console.log(oneTwoHundred);
+console.log(sortedArr(oneTwoHundred));
+console.log(biggestTen(sortedArr(oneTwoHundred)));
+console.log(getAvg(biggestTen(sortedArr(oneTwoHundred))));
